258fd2b73509e05fe3a45fd5f0e906c571831124
PanosGiazi <33422976+PanosGiazi@users.noreply.github.com>
2017-11-21 15:58:24 +0200

    initializes project

---
+++ Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine($"Value_1 :{a}\tValue_2 :{b}");
+            Utilities.Swap(ref a, ref b);
+            Console.WriteLine($"Value_1 :{a}\tValue_2 :{b}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public static class Utilities
+    {
+        public static void Swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+        
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+        }
+    }
+}
